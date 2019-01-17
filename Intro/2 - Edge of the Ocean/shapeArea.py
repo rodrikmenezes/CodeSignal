2aa0cb47ba6144a4cea5f2d1b608d8aa06d55cfc
Rodrik José Menezes <45806193+rodrikmenezes@users.noreply.github.com>
2019-01-17 19:04:19 -0200

    Inserção de cabeçalho

--- Intro/2 - Edge of the Ocean/shapeArea.py
+++ Intro/2 - Edge of the Ocean/shapeArea.py
@@ -1,3 +1,7 @@
+""" DESCRIÇÃO DO DESAFIO
+O desafio é escrever uma função onde se recebe como input um número natural e como output a área correspondente
+que descrita pela figura area areaShape.png em anexo"""
+
 def shapeArea(n):
     lateral = n + (n - 1)
     soma = lateral
