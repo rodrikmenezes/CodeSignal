72f4bdb99cf0d0da6fac07d544b14a44b40ebc4d
rodrikmenezes <rodrikaraujo@gmail.com>
2019-07-30 00:29:29 -0300

    Finalizado exercício 8

--- Intro/2 - Edge of the Ocean/8 matrixElementsSum.py
+++ Intro/2 - Edge of the Ocean/8 matrixElementsSum.py
@@ -3,26 +3,48 @@
 # Somar todos os valores que não aparecem acima de um 'zero' da matriz fornecida
 # =============================================================================
 
-# output: 9
-matrix = [[1, 1, 1, 0], 
+
+import numpy as np
+
+def matrixElementsSum(matrix):
+    
+    matrix_clone = matrix[:]
+    x = len(matrix)
+    y = len(matrix[0])
+    
+    for i in range(1, x):
+        for j in range(y):
+            if matrix[i-1][j] == 0:
+                matrix_clone[i][j] = 0
+    
+    matrix_final = np.array(matrix_clone)
+    return sum(sum(matrix_final))
+
+
+# TESTES
+
+testes = [0] * 2
+resultados_esperados = [0] * 2
+respostas = []
+comparacao = []
+
+testes[0] = [[0, 1, 1, 2], 
+          [0, 5, 0, 0], 
+          [2, 0, 3, 3]]
+resultados_esperados[0] = 9
+
+testes[1] = [[1, 1, 1, 0], 
           [0, 5, 0, 1], 
           [2, 1, 3, 10]]
+resultados_esperados[1] = 9
 
-x = len(matrix)
-y = len(matrix[0])
-zeros = []
-soma_total = 0
-soma_zeros = 0
-for i in range(1, x):
-        
-        for j in range(y):
-            soma_total += matrix[i][j]
-            if matrix[i-1][j] == 0:
-                print(matrix[i][j])
-                zeros.append(j)
-                soma_zeros += matrix[i][j]
-
-#for i in range(1, len(matrix)):
-#    for j in matrix[i]:
-#        if matrix[i-1] != 0:
-#            print(j)
+n = 0
+for teste in testes:
+    respostas.append(matrixElementsSum(teste))
+    if respostas[n] == resultados_esperados[n]:
+        comparacao.append('OK')
+    else:
+        comparacao.append('ERRO!')    
+    print('teste', n+1,'- resultado esperado:', resultados_esperados[n],
+          '  resultado do programa:', respostas[n], comparacao[n])
+    n += 1
